02b80d4040f881e744e4406f744aad80e3881251
Luca Del Puppo <luca@delpuppo.net>
2025-07-17 10:46:39 +0200

    chore: init project

---
+++ packages/common/dist/main.d.ts
@@ -0,0 +1,3 @@
+export declare const isEmpty: (data: unknown) => data is null | undefined;
+export declare const isObject: (data: unknown) => unknown;
+export declare const isBlank: (data: unknown) => {};
